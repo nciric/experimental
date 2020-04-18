25c673b6785dd4127b968fd454a47147e8bf9e1b
Nebojša Ćirić <nciric@gmail.com>
2020-04-18 15:40:31 -0700

    Pass strings in, ints out ffi.

--- ffi/main.cc
+++ ffi/main.cc
@@ -1,13 +1,17 @@
 #include "struct_printer.h"
 
+#include <iostream>
+
 int main() {
     PrintableStruct example;
 
+    example.text_ptr = "Nebojsa Ciric";
     example.short_number = 4;
     example.long_number = 0x100000L;
     example.logical = false;
 
-    struct_printer(example);
+    uint32_t count = struct_printer(example);
+    std::cout << "From C++, count elements: " << count << std::endl;
 
     return 0;
 }
--- ffi/struct_printer/src/lib.rs
+++ ffi/struct_printer/src/lib.rs
@@ -1,9 +1,10 @@
-// use std::os::raw::c_char;
+use std::os::raw::c_char;
+use std::ffi::CStr;
 
 #[repr(C)]
 // Struct to be printed.
 pub struct PrintableStruct {
-//    pub text_ptr: *const c_char,
+    pub text_ptr: *const c_char,
     pub logical: bool,
     pub short_number: i8,
     pub long_number: i32,
@@ -11,8 +12,20 @@ pub struct PrintableStruct {
 
 #[no_mangle]
 // Prints all of the struct fields.
-pub extern "C" fn struct_printer(printable_struct: PrintableStruct) {
+pub extern "C" fn struct_printer(printable_struct: PrintableStruct) -> u32 {
+    // We have to be careful with C strings
+    let c_str = unsafe {
+        assert!(!printable_struct.text_ptr.is_null());
+        CStr::from_ptr(printable_struct.text_ptr)
+    };
+    let r_str = c_str.to_str().unwrap();
+
+    println!("From Rust, content of struct is:");
+    println!("Text        : {}", r_str);
     println!("Logical     : {}", printable_struct.logical);
     println!("Short number: {}", printable_struct.short_number);
     println!("Long number : {}", printable_struct.long_number);
+
+    // Number of fields in the struct.
+    return 4;
 }
